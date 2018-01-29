6930612f148af7c2c1303a9756826a8147bea06a
Dennis Mensink <dennitium@gmail.com>
2018-01-29 23:22:35 +0100

    Create TempCourse.R

---
+++ TempCourse.R
@@ -0,0 +1,23 @@
+library(dslabs)
+data("murders")
+str(murders)
+
+# We extract the population like this:
+p <- murders$population
+
+# This is how we do the same with the square brackets:
+o <- murders[["population"]] 
+
+# We can confirm these two are the same
+identical(o, p)
+
+# Use square brackets to extract `abb` from `murders` and assign it to b
+
+# Check if `a` and `b` are identical 
+a <- murders[["population"]]
+b <- murders[["abb"]]
+identical(a, b)
+
+length(level(murders$region))
+
+table(murders$region)
