1c6b14f63c5262d732b4030c1fae44e35e20053d
Dennis Mensink <dennitium@gmail.com>
2018-01-30 22:55:41 +0100

    Create VectorCoercion.R

---
+++ VectorCoercion.R
@@ -0,0 +1,19 @@
+# input of x = int, str, int
+x <- c(1,"cancada",3)
+# output the content of x
+print(x)
+# output the class of x
+class(x)
+
+# input int 1:5
+x <- 1:5
+# convert to character class
+y <- as.character(x)
+# output the content of y
+print(y)
+# convert to numeric class
+as.numeric(y)
+
+# input of x str then convert to numeric
+x <- c("1","b","2")
+as.numeric(x)
