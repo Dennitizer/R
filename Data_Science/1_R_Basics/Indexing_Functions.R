44ea094b0611d119605c4481fc4fac3f53fdcd45
Dennis Mensink <dennitium@gmail.com>
2018-02-12 22:37:39 +0100

    Add files via upload

---
+++ Data_Science/1_R_Basics/Indexing_Functions.R
@@ -0,0 +1,22 @@
+library(dslabs)
+
+x <- c(FALSE,TRUE,FALSE,TRUE,TRUE,FALSE)
+
+which(x)
+
+index <- which(murders$state == "Massachusetts")
+index
+murder_rate[index]
+
+index <- match(c("New York", "Florida", "Texas"),murders$state)
+index
+
+murders$state[index]
+murder_rate[index]
+
+x <- c("a", "b", "c", "d", "e")
+y <- c("a", "d", "f")
+y %in% x
+
+c("Boston", "Dakota", "Washington") %in% murders$state
+
